20a1f711b16903d0f86ff41da25b78271dd34902
Braulio <braulio.sassi@gmail.com>
2023-08-24 16:01:35 -0300

    P2 - Conclusão

--- event/event/src/app/app.component.ts
+++ event/event/src/app/app.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  pressFunction(){
+    alert('You just pressed a key')
+  }
+
+  upFunction() {
+      console.log('You just released the key you pressed')
+  }
+
+  downFunction() {
+    console.log('You jus depresseda keyboard button')
+  }
+
+  clickFunction() {
+    alert('You just clicked a button')
+  }
 }
